068c253bb5b74f4d49e7459b79b34ee4c2570fb2
Friedrich Gaschler <fritz@promed.at>
2025-03-25 13:35:09 +0100

    new way to restore db

--- restore_dabase.sh
+++ restore_dabase.sh
@@ -1,1 +1,13 @@
-python cli.py restore-db --path-to-dump ./db/dump.sql --db-user fritz && python cli.py modify-user -u friedrich@gaschler.at -p test --disable-2fa
+#!/usr/bin/env python3
+
+import platform
+import subprocess
+
+system = platform.system()
+
+if system == 'Darwin':  # Note: platform.system() returns 'Darwin' on macOS
+    subprocess.run(["python", "cli.py", "restore-db", "--path-to-dump", "./db/dump.sql", "--db-user", "fritz"])
+elif system == 'Linux':
+    subprocess.run(["python", "cli.py", "restore-db", "--path-to-dump", "./db/dump.sql", "--db-user", "postgres"])
+
+subprocess.run(["python", "cli.py", "modify-user", "-u", "friedrich@gaschler.at", "-p", "test", "--disable-2fa"])
